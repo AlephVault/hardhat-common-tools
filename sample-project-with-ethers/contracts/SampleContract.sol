cfc8a47853eb1358866dae980a7f0ff3d23536b9
Luis Masuelli <luisfmasuelli@gmail.com>
2025-02-02 11:56:31 -0500

    Fixing the sample module and contract.

--- sample-project-with-ethers/contracts/SampleContract.sol
+++ sample-project-with-ethers/contracts/SampleContract.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.10;
 contract SampleContract {
     constructor(){}
 
-    event SampleEvent(bytes32 indexed foo, uint256 indexed bar, int256 indexed baz, bytes data);
+    event SampleEvent(bytes32 indexed foo, uint256 indexed bar, int256 indexed baz, string data);
 
-    function fireSampleEvent(bytes32 foo, uint256 bar, int256 baz, bytes memory data) external {
+    function fireSampleEvent(bytes32 foo, uint256 bar, int256 baz, string memory data) external {
         emit SampleEvent(foo, bar, baz, data);
     }
 }
--- sample-project-with-ethers/ignition/modules/SampleContract.js
+++ sample-project-with-ethers/ignition/modules/SampleContract.js
@@ -6,35 +6,35 @@ module.exports = buildModule("SampleContractModule", (m) => {
   m.call(sampleContract, "fireSampleEvent", [
     "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
     1, -1, "Hello World (A)"
-  ]);
+  ], {id: "fireSampleEvent1"});
   m.call(sampleContract, "fireSampleEvent", [
     "0xf0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcde",
     2, -2, "Hello World (B)"
-  ]);
+  ], {id: "fireSampleEvent2"});
   m.call(sampleContract, "fireSampleEvent", [
     "0xef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcd",
     3, -3, "Hello World (C)"
-  ]);
+  ], {id: "fireSampleEvent3"});
   m.call(sampleContract, "fireSampleEvent", [
     "0xdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abc",
     4, -4, "Hello World (D)"
-  ]);
+  ], {id: "fireSampleEvent4"});
   m.call(sampleContract, "fireSampleEvent", [
     "0xcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789ab",
     5, -5, "Hello World (E)"
-  ]);
+  ], {id: "fireSampleEvent5"});
   m.call(sampleContract, "fireSampleEvent", [
     "0xbcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789a",
     6, -6, "Hello World (F)"
-  ]);
+  ], {id: "fireSampleEvent6"});
   m.call(sampleContract, "fireSampleEvent", [
     "0xabcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789",
     7, -7, "Hello World (G)"
-  ]);
+  ], {id: "fireSampleEvent7"});
   m.call(sampleContract, "fireSampleEvent", [
     "0x9abcdef0123456789abcdef0123456789abcdef0123456789abcdef012345678",
     8, -8, "Hello World (H)"
-  ]);
+  ], {id: "fireSampleEvent8"});
 
   return { sampleContract };
 });
