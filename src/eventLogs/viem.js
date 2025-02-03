ad1a51f2daeeec3cda951afa4c28158fcefe815f
Luis Masuelli <luisfmasuelli@gmail.com>
2025-02-02 23:16:07 -0500

    Removing some unused functions from the viem file for logs.

--- src/eventLogs/viem.js
+++ src/eventLogs/viem.js
@@ -176,66 +176,6 @@ function normalizeLog(abi, log) {
     }
 }
 
-// Encodes the given values to be used as topics.
-function encodeTopics(hre, eventFragment, indexedArgs) {
-    const topics = [];
-    let index = 0;
-    for (const param of eventFragment.inputs) {
-        if (param.indexed) {
-            const value = indexedArgs[index++];
-            if (value !== undefined) {
-                topics.push(encodeIndexedValue(hre, param.type, value));
-            } else {
-                topics.push(null);
-            }
-        }
-    }
-    return topics;
-}
-
-const uintTypes = [];
-const intTypes = [];
-const bytesTypes = [];
-
-for(let j = 1; j <= 32; j++) {
-    uintTypes.push(`uint${j * 8}`);
-    intTypes.push(`int${j * 8}`);
-    bytesTypes.push(`bytes${j}`);
-}
-
-// Encodes an indexed value.
-function encodeIndexedValue(hre, type, value, cannotBeArrayOrNull) {
-    const ethers = hre.ethers;
-
-    // If array/null are allowed by this point, test
-    // for arrays or null values and return appropriately.
-    cannotBeArrayOrNull ||= false;
-    if (!cannotBeArrayOrNull) {
-        if (value === null) return null;
-        if (Array.isArray(value)) return value.map((v) => encodeIndexedValue(type, v, true));
-    }
-
-    // By this point, this null check will only occur
-    // if array/null values is not allowed. In this
-    // case, an error will be thrown.
-    if (value === null) throw new Error(`null cannot be an individual topic in an array`);
-
-    // By this point, we don't care about arrays or
-    // null values, but test against the types instead.
-    if (type === "address") {
-        return ethers.zeroPadValue(value, 32); // Address encoding
-    } else if (uintTypes.indexOf(type) >= 0) {
-        return ethers.zeroPadValue(ethers.toBeHex(value, 32), 32); // Uint encoding
-    } else if (intTypes.indexOf(type) >= 0) {
-        return ethers.zeroPadValue(ethers.toBeHex(ethers.toTwos(value, 256), 32), 32); // Signed int encoding
-    } else if (type === "bool") {
-        return ethers.zeroPadValue(ethers.toBeHex(value ? 1 : 0, 32), 32); // Boolean encoding (true = 1, false = 0)
-    } else if (bytesTypes.indexOf(type) >= 0) {
-        return ethers.zeroPadValue(value, 32); // Zero-pad fixed bytes types (bytes1 to bytes32)
-    }
-    throw new Error(`Unsupported indexed type: ${type}`);
-}
-
 module.exports = {
     fetchLogs, watchLogs, unWatchLogs
 }
